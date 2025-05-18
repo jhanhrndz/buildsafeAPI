5996903d2afd8485e12190a04e5cb7fa6f713ea3
Nasor2 <penaortegasamuel@gmail.com>
2025-05-18 15:20:44 -0500

    credetials: true added

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express();
 // 📦 Middlewares globales
 app.use(cors(
   {
-  origin: 'http://localhost:5173', // URL de tu frontend
+  origin: 'http://localhost:5173',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }
